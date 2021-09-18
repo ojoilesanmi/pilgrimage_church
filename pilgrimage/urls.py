9f7e9532fe9e651c0d477ac8881f9cb5a409bd76
Ojo Ilesanmi <ojoilesanmi89@gmail.com>
2021-09-18 12:55:32 +0100

    Added to the welcome address

--- pilgrimage/urls.py
+++ pilgrimage/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pilgrimage_app.urls')),
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
 ]
